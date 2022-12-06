c40cd9450bb27669652a3b49e445ab76eb04057d
ysalaunstudent <ysalaunstudent@gmail.com>
2022-12-06 10:22:50 +0100

    input() added to get the user input on the city name

--- Meteo_vocal_assistant/main.py
+++ Meteo_vocal_assistant/main.py
@@ -4,7 +4,7 @@ import json
 
 api_key = "my_api_key"
 
-city = input("enter the city you want the weather for")
+city = input("enter the city you want the weather for: ")
 
 try:
     response = requests.request('GET', f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
@@ -15,8 +15,6 @@ if response.status_code != 200:
     print('An error occurred please try again !')
 else:
     data = json.loads(response.text)
-
-    print(data)
 
     temperature = data['main']['temp']
     feels_like = data['main']['feels_like']
